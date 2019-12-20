8f043b375e0dbb51a1c170bc8f8141f6221cc79a
Eryk Rachwal <erykrachwal@yahoo.com>
2019-12-20 15:54:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,6 @@ def long_planeteer_calls(words)
   words.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+#cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese
 end
